03a3c8c3fd535bc0a1d7c059cd7d7c4ef0d99f34
jbn179 <9772@holbertonstudents.com>
2025-08-29 12:59:43 +0200

    add minimum-operations project

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -8,26 +8,26 @@ def minOperations(n):
     """
     Calculates the fewest number of operations needed to result in exactly
     n H characters in the file.
-    
+
     Args:
         n: The target number of H characters
-        
+ 
     Returns:
         Integer representing minimum operations, or 0 if impossible
     """
     if n <= 1:
         return 0
-    
+
     operations = 0
     factor = 2
-    
+
     while factor * factor <= n:
         while n % factor == 0:
             operations += factor
             n //= factor
         factor += 1
-    
+
     if n > 1:
         operations += n
-        
+ 
     return operations
